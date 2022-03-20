b7ea09647973d258b63f9b99a3f284790fe145b8
Luke <lukestumfall@gmail.com>
2022-03-19 20:42:48 -1000

    finished exerize 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function (x, y) {
+    if (x > 0 && y > 0 && typeof x === 'number' && typeof y === 'number') {
+        var valorx = x;
+        var valory = y;
+        var total = 0;
+        if (x < y) {
+            for (var i = valorx; i <= valory; i++) {
+                total += i;
+            }
+            return total;
+        } else if (x > y) {
+            for (var i = valory; i <= valorx; i++) {
+                total += i;
+            }
+            return total;
+        }
+    } else {
+        return 'ERROR'
+     }
+    }
 // Do not edit below this line
 module.exports = sumAll;
