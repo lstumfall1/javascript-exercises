c6862331803a3c0988ee186fc3a36fd472b07db9
Luke <lukestumfall@gmail.com>
2022-03-19 20:59:50 -1000

    finished exerize 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fNum) {
+let cConversion = (fNum - 32) * (5 / 9)
+let cRounded = Math.round(cConversion * 10) / 10;
+return cRounded
 };
 
-const ctof = function() {
-
+const ctof = function(cNum) {
+  let fConversion = (cNum * (9 / 5) + 32)
+  let fRounded = Math.round(fConversion * 10) / 10;
+  return fRounded
 };
 
 // Do not edit below this line
