355b3869f61975fb6e9130ff34d7fed791c379c1
Luke <lukestumfall@gmail.com>
2022-03-19 20:52:00 -1000

    finished exerize 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
+const leapYears =function (year)
+{
+    return (year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
